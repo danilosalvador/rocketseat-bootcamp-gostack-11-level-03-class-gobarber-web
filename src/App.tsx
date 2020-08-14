783c0d4590f8bde79640ff11744de05b15c3f24b
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-08-14 01:07:44 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <h1>GoBarber</h1>
+  <>
+    <h1>GoBarber</h1>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
