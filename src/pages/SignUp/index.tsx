1d66749959a3e620e6e2092119d04dd3121b4081
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-08-17 22:58:20 -0300

    Página de Cadastro

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FiUser, FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content, Background } from './styles';
+
+import imgLogo from '../../assets/logo.svg';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={imgLogo} alt="GoBabaer" />
+      <form>
+        <h1>Faça seu cadastro</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="Email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="Password"
+          type="password"
+          icon={FiLock}
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+      <a href="back">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
