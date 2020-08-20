564d1c52bbe067d96f1a090aa6b5cf0ea271b29f
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-08-19 23:58:29 -0300

    API de Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
-    {/* <SignUp /> */}
+    <AuthContext.Provider value={{ name: 'Danilo Salvador' }}>
+      <SignIn />
+      {/* <SignUp /> */}
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
@@ -8,6 +8,7 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import getValidationErrors from '../../utils/getValidationErrors';
+import AuthContext from '../../context/AuthContext';
 
 import { Container, Content, Background } from './styles';
 
@@ -15,6 +16,10 @@ import imgLogo from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
+
+  console.log(name);
 
   const handleSubmit = useCallback(async (data: any) => {
     try {
