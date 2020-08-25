6294e06d202fbf54645c8cf2610122ad51ec3123
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-08-25 00:15:10 -0300

    Estrutura de Toasts

---
+++ src/components/ToastContainer/styles.ts
@@ -0,0 +1,81 @@
+import styled, { css } from 'styled-components';
+
+interface ToastProps {
+  type?: 'info' | 'success' | 'error';
+  hasDescription: boolean;
+}
+
+const toastTypeVariant = {
+  info: css`
+    background: #ebf8ff;
+    color: #3172b2;
+  `,
+  success: css`
+    background: #e6fffa;
+    color: #2e656a;
+  `,
+  error: css`
+    background: #fddede;
+    color: #c53030;
+  `,
+};
+
+export const Container = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 30px;
+  overflow: hidden;
+`;
+
+export const Toast = styled.div<ToastProps>`
+  width: 360px;
+
+  position: relative;
+  padding: 16px 30px 16px 16px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+
+  ${props => toastTypeVariant[props.type || 'info']}
+
+  & + div {
+    margin-top: 14px;
+  }
+
+  > svg {
+    margin: 1px 12px 0 0;
+  }
+
+  div {
+    flex: 1;
+
+    p {
+      margin-top: 4px;
+      font-size: 14px;
+      opacity: 0.8;
+      line-height: 20px;
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 13px;
+    top: 16px;
+    opacity: 0.6;
+    color: inherit;
+    border: 0;
+    background: transparent;
+  }
+
+  ${props =>
+    !props.hasDescription &&
+    css`
+      align-items: center;
+
+      svg {
+        margin-top: 0;
+      }
+    `}
+`;
